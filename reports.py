9245cddf20b738e31b340e006a4fa20d47ab3024
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-26 17:30:07 +0200

    get_latest function working, added a new function called 'games_to_list'

--- reports.py
+++ reports.py
@@ -4,6 +4,13 @@ year = 0
 games = []
 
 
+def games_to_lists(file_name):
+    global games
+    with open(file_name) as stats:
+        for line in csv.reader(stats, dialect="excel-tab"):
+            games.append(line)
+
+
 def count_games(file_name):
     result = sum(1 for line in open(file_name))
     return result
@@ -14,3 +21,15 @@ def decide(file_name, year):
         return True
     else:
         return False
+
+
+def get_latest(file_name):
+    games_to_lists(file_name)
+    latest = 0
+    for iteration in range(len(games)):
+        if int(games[iteration][2]) > int(latest):
+            latest = games[iteration][2]
+    for iteration in range(len(games)):
+        if games[iteration][2] == latest:
+            result = str(games[iteration][0])
+            return result
