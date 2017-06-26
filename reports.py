533c72ce887b8a964a787a3d93203c3e0d773d18
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-26 17:00:08 +0200

    decide and count_games functions working

--- reports.py
+++ reports.py
@@ -1,2 +1,16 @@
-
+import csv
 # Report functions
+year = 0
+games = []
+
+
+def count_games(file_name):
+    result = sum(1 for line in open(file_name))
+    return result
+
+
+def decide(file_name, year):
+    if str(year) in open(file_name).read():
+        return True
+    else:
+        return False
