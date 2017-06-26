860e96c8869aa6614724b712c479a2f490563474
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-26 20:55:17 +0200

    Top sold FPS function added

--- reports.py
+++ reports.py
@@ -51,3 +51,29 @@ def get_line_number_by_title(file_name, title):
             result = int(iteration + 1)
             break
     return result
+
+
+def get_genres(file_name):
+    result = []
+    games = games_to_list(file_name)
+    for iteration in range(len(games)):
+        if games[iteration][3] not in result:
+            result.append(games[iteration][3])
+    result = sorted(result, key=str.lower)
+    return result
+
+
+def when_was_top_sold_fps(file_name):
+    games = games_to_list(file_name)
+    best_seller = 0
+    fpses = []
+    for iteration in range(len(games)): 
+        if games[iteration][3] == "First-person shooter": 
+            fpses.append(games[iteration])
+    for iteration in range(len(fpses)):
+        if float(fpses[iteration][1]) > float(best_seller):
+            (best_seller) = (fpses[iteration][1])
+    for iteration in range(len(fpses)):
+        if best_seller == fpses[iteration][1]:
+            result = int(fpses[iteration][2])
+            return result
