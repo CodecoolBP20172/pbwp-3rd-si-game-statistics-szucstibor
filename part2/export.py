ea158872c8cf98ac46e822f119a2fad544252054
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-27 11:17:15 +0200

    Exporting data into a single file.

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,15 @@
-
+from reports import *
 # Export functions
+
+
+def export(file_name, export_to):
+    with open(export_to, "w") as export:
+        export.writelines(str(games_to_list(file_name))+"\n")
+        export.writelines(str(get_most_played(file_name))+"\n")
+        export.writelines(str(sum_sold(file_name))+"\n")
+        export.writelines(str(get_selling_avg(file_name))+"\n")
+        export.writelines(str(count_longest_title(file_name))+"\n")
+        export.writelines(str(get_date_avg(file_name))+"\n")
+        export.writelines(str(get_game(file_name, "Terraria"))+"\n")
+
+export("game_stat.txt", "export.txt")
