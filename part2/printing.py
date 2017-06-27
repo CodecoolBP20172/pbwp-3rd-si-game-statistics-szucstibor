4cb24e201b9766eef6d64430298a9f6ddbef23e7
Szűcs Tibor <job.szucstibor@gmail.com>
2017-06-27 11:19:05 +0200

    Printing the data given by the report.py

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,16 @@
-
+from reports import *
+import pprint
 # Printing functions
+
+
+def pretty_print(file_name):
+    pp = pprint.PrettyPrinter(width=140, compact=True)
+    pp.pprint(str(games_to_list(file_name)))
+    pp.pprint(str(get_most_played(file_name)))
+    pp.pprint(str(sum_sold(file_name)))
+    pp.pprint(str(get_selling_avg(file_name)))
+    pp.pprint(str(count_longest_title(file_name)))
+    pp.pprint(str(get_date_avg(file_name)))
+    pp.pprint(str(get_game(file_name, "Terraria")))
+
+pretty_print("game_stat.txt")
